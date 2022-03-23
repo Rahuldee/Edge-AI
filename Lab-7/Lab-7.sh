eeaa1f170fcc0e2731ba0633bb34e962f42618e3
Rahuldee <Rahuldee@gmail.com>
2022-03-22 21:23:09 -0700

    Update Lab-7.sh

--- Lab-7/Lab-7.sh
+++ Lab-7/Lab-7.sh
@@ -5,13 +5,14 @@ cat /etc/os-release
 
 # Update packages on your Raspberry Pi OS.
 sudo apt-get update
+sudo apt-get install python3-venv
 
 # Check your Python version. You should have Python 3.7 or later.
 python3 --version
 
 # Install virtualenv and upgrade pip.
 python3 -m pip install --user --upgrade pip
-python3 -m pip install --user virtualenv
+#python3 -m pip install --user virtualenv
 
 # Create a Python virtual environment for the TFLite samples (optional but strongly recommended)
 python3 -m venv ~/tflite
@@ -20,8 +21,6 @@ python3 -m venv ~/tflite
 source ~/tflite/bin/activate
 
 # Clone the TensorFlow example repository with the TFLite Raspberry Pi samples.
-git clone https://github.com/Rahuldee/Edge-AI/Lab-7
-cd Lab-7
 
 # Install dependencies required by the sample
 pip3 install tflite-runtime
