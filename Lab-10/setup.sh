ffbb9b23559132048ba78bfef8e78b593428dda3
Rahuldee <Rahuldee@gmail.com>
2022-03-24 15:40:37 -0700

    Update setup.sh

--- Lab-10/setup.sh
+++ Lab-10/setup.sh
@@ -17,25 +17,4 @@ sudo apt-get install libedgetpu1-std
 #install PyCoral as follows
 sudo apt-get install python3-pycoral
 
-# Check your Python version. You should have Python 3.7 or later.
-python3 --version
 
-# Install virtualenv and upgrade pip.
-python3 -m pip install --user --upgrade pip
-python3 -m pip install --user virtualenv
-
-# Create a Python virtual environment for the TFLite samples (optional but strongly recommended)
-python3 -m venv ~/accelerate
-
-# Run this command whenever you open a new Terminal window/tab to activate the environment.
-source ~/accelerate/bin/activate
-
-# Clone the TensorFlow example repository with the TFLite Raspberry Pi samples.
-git clone https://github.com/Rahuldee/Edge-AI/Lab-10
-cd Lab-10
-
-# Install dependencies required by the sample
-pip3 install tflite-runtime
-pip3 install Pillow
-
-# Run the object detection sample
